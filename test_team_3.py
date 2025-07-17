c25d12197eb70291baa820aa1b5b2eb734230156
Ryan Bolt <114034672+boltydev@users.noreply.github.com>
2025-07-17 01:26:05 +0000

    Add US_DRIVER_LICENSE

--- test_team_3.py
+++ test_team_3.py
@@ -16,7 +16,14 @@ class TestTeam_3(unittest.TestCase):
         """Test US_BANK_NUMBER functionality"""
 
     def test_us_driver_license(self):
-        """Test US_DRIVER_LICENSE functionality"""
+
+        # Positive Test case
+        result = analyze_text('My Drivers License number is 0000123122424' , ['US_DRIVER_LICENSE'])
+        print (result)
+
+        # Negative test case
+        result = analyze_text('My Drivers License number is XYZ999' , ['US_DRIVER_LICENSE'])
+        self.assertEqual(len(result), 0, 'Invalid License Number')
 
     def test_us_itin(self):
         """Test US_ITIN functionality"""
