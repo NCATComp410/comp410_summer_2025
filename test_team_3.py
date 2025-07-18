c4a0e27f2f3d941a044eb8f56b41f9741290ef03
Robert Reid <Robsrj17@gmail.com>
2025-07-18 00:33:30 +0000

    add US_SSN test case

--- test_team_3.py
+++ test_team_3.py
@@ -14,6 +14,20 @@ class TestTeam_3(unittest.TestCase):
 
     def test_us_bank_number(self):
         """Test US_BANK_NUMBER functionality"""
+        #Positive test case
+    test_str = "Bank Accounts 89833344476 and 11133345623 have the money in there."
+    result = analyze_text(test_str,['US_BN'] )
+    self.assertEqual(len(result),0), ('Should have found 0 Bank Numbers')
+       #Check that a BN was found
+    for entity in result:
+            self.assertEqual(entity.entity_type, 'Us_BN')
+    #Negative test case
+    test_str = "Number 201213"
+    result = analyze_text(test_str,['US_BN'] )
+    self.assertEqual(len(result),0), ('Should have found 0 Bank Numbers')
+
+
+           
 
     def test_us_driver_license(self):
         """Test US_DRIVER_LICENSE functionality"""
